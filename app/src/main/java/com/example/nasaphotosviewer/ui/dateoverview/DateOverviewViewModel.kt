fd4886115c81ae6d9d8b9e8630c48d84138bd4a8
apolloandroid <hmelevskih94@gmail.com>
2020-08-02 22:20:15 +0300

    Created DateOverviewViewModel class with initializing viewModel coroutine scope and LiveData

--- app/src/main/java/com/example/nasaphotosviewer/ui/dateoverview/DateOverviewViewModel.kt
+++ app/src/main/java/com/example/nasaphotosviewer/ui/dateoverview/DateOverviewViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.nasaphotosviewer.ui.dateoverview
 
-class DateOverviewViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.nasaphotosviewer.data.model.DateResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+
+class DateOverviewViewModel(application: Application) : AndroidViewModel(application) {
+    private val viewModelJob = Job()
+    private val viewModelScope = CoroutineScope(Dispatchers.IO + Job())
+
+    private val _datesList: MutableLiveData<List<DateResponse>> = getDatesList()
+    val datesList: LiveData<List<DateResponse>>
+        get() = _datesList
+
+    private fun getDatesList(): MutableLiveData<List<DateResponse>> {
+        return MutableLiveData()
+    }
+
 }
