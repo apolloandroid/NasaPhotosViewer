b967ddc0c1db0c839058f6fbff1bca81af734907
apolloandroid <hmelevskih94@gmail.com>
2020-08-02 22:20:57 +0300

    Created NasaService class

--- app/src/main/java/com/example/nasaphotosviewer/data/network/NasaService.kt
+++ app/src/main/java/com/example/nasaphotosviewer/data/network/NasaService.kt
@@ -1,11 +1,11 @@
 package com.example.nasaphotosviewer.data.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
@@ -25,7 +25,7 @@ class NasaService {
             .baseUrl("https://api.nasa.gov/EPIC/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .client(createOkHttpClient())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
     }
 
