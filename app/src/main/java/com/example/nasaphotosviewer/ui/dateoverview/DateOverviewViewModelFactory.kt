c29a2f4a6cd0569103933bde6474d2b452592feb
apolloandroid <hmelevskih94@gmail.com>
2020-08-04 00:06:55 +0300

    Did some refactoring

--- app/src/main/java/com/example/nasaphotosviewer/ui/dateoverview/DateOverviewViewModelFactory.kt
+++ app/src/main/java/com/example/nasaphotosviewer/ui/dateoverview/DateOverviewViewModelFactory.kt
@@ -3,9 +3,10 @@ package com.example.nasaphotosviewer.ui.dateoverview
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.nasaphotosviewer.App
 import java.lang.IllegalArgumentException
 
-class DateOverviewViewModelFactory(private val application: Application) :
+class DateOverviewViewModelFactory(private val application: App) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DateOverviewViewModel::class.java)) {
