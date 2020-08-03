7f2bcf2680b428a4e4fdd6df88e72d996562bb79
apolloandroid <hmelevskih94@gmail.com>
2020-08-04 00:07:23 +0300

    Edited type of response object

--- app/src/main/java/com/example/nasaphotosviewer/data/network/NasaApi.kt
+++ app/src/main/java/com/example/nasaphotosviewer/data/network/NasaApi.kt
@@ -1,16 +1,16 @@
 package com.example.nasaphotosviewer.data.network
 
-import com.example.nasaphotosviewer.data.model.DateResponse
-import com.example.nasaphotosviewer.data.model.PhotoResponse
-import io.reactivex.Single
+import com.example.nasaphotosviewer.data.model.Date
+import com.example.nasaphotosviewer.data.model.Photo
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface NasaApi {
 
     @GET("natural/all")
-    fun getDatesWithPhoto(): Single<List<DateResponse>?>?
+    fun getDatesWithPhoto(): Call<List<Date>>
 
     @GET("natural/date/{date}")
-    fun getPhotosForDate(@Path("date") date: String?): Single<List<PhotoResponse>?>?
+    fun getPhotosForDate(@Path("date") date: String?): Call<List<Photo>>
 }
