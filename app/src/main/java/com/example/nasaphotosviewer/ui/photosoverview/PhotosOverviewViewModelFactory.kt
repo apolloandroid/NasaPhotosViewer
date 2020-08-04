fe30c85c791038e06f7006003fad11f8577dc4df
apolloandroid <hmelevskih94@gmail.com>
2020-08-05 00:23:26 +0300

    Created factory for PhotosOverviewFragment

---
+++ app/src/main/java/com/example/nasaphotosviewer/ui/photosoverview/PhotosOverviewViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.nasaphotosviewer.ui.photosoverview
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.nasaphotosviewer.App
+import com.example.nasaphotosviewer.ui.dateoverview.DateOverviewViewModel
+import java.lang.IllegalArgumentException
+
+class PhotosOverviewViewModelFactory(private val application: App) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PhotosOverviewViewModel::class.java)) {
+            return PhotosOverviewViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
