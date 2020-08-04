105c684c772400276d64d8e4a96de9939adc0ffc
apolloandroid <hmelevskih94@gmail.com>
2020-08-05 00:23:04 +0300

    Created PhotosOverviewFragment

--- app/src/main/java/com/example/nasaphotosviewer/ui/photosoverview/PhotosOverviewFragment.kt
+++ app/src/main/java/com/example/nasaphotosviewer/ui/photosoverview/PhotosOverviewFragment.kt
@@ -5,15 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.nasaphotosviewer.App
 import com.example.nasaphotosviewer.R
+import com.example.nasaphotosviewer.databinding.FragmentPhotosOverviewBinding
+import com.example.nasaphotosviewer.ui.dateoverview.DateListAdapter
 
 
 class PhotosOverviewFragment : Fragment() {
+
+    private val viewModel: PhotosOverviewViewModel by lazy { initViewModel() }
+    private lateinit var binding: FragmentPhotosOverviewBinding
+    private var photosListAdapter = PhotosListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_photos_overview, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_photos_overview, container, false)
+        initPhotosList()
+
+        viewModel.photos.observe(this, Observer {
+            photosListAdapter.submitList(it)
+        })
+
+        viewModel.photoClicked.observe(this, Observer {
+            if (it) navigateToPhotoDetailsFragment()
+        })
+
+        return binding.root
+    }
+
+    private fun initViewModel(): PhotosOverviewViewModel {
+        val application = App()
+        val photosOverviewViewModelFactory = PhotosOverviewViewModelFactory(application)
+        return photosOverviewViewModelFactory.create(PhotosOverviewViewModel::class.java)
+    }
+
+    private fun initPhotosList() {
+        binding.photoList.adapter = photosListAdapter
+        binding.photoList.setHasFixedSize(true)
+    }
+
+    private fun navigateToPhotoDetailsFragment() {
+        findNavController().navigate(R.id.action_photosOverviewFragment_to_photoDetailsFragment)
     }
 }
