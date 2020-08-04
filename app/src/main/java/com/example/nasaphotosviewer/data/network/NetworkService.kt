3aa25295d8bba555f6f514402079e7fc8b030610
apolloandroid <hmelevskih94@gmail.com>
2020-08-04 23:32:43 +0300

    Added interface for working with NASA API

---
+++ app/src/main/java/com/example/nasaphotosviewer/data/network/NetworkService.kt
@@ -0,0 +1,9 @@
+package com.example.nasaphotosviewer.data.network
+
+import com.example.nasaphotosviewer.data.model.Date
+import com.example.nasaphotosviewer.data.model.Photo
+
+interface NetworkService {
+    fun getDates(): List<Date>?
+    fun getPhotosForDate(): List<Photo>?
+}
