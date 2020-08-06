0a5241563f59d7e202d9b13bb6ec649ef17be5f8
apolloandroid <hmelevskih94@gmail.com>
2020-08-07 00:17:18 +0300

    Did some refactoring

--- app/src/main/java/com/example/nasaphotosviewer/ui/photosoverview/PhotosOverviewViewModelFactory.kt
+++ app/src/main/java/com/example/nasaphotosviewer/ui/photosoverview/PhotosOverviewViewModelFactory.kt
@@ -3,14 +3,13 @@ package com.example.nasaphotosviewer.ui.photosoverview
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.nasaphotosviewer.App
-import com.example.nasaphotosviewer.ui.dateoverview.DateOverviewViewModel
 import java.lang.IllegalArgumentException
 
-class PhotosOverviewViewModelFactory(private val application: App) :
+class PhotosOverviewViewModelFactory(private val application: App, private val photo:String) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PhotosOverviewViewModel::class.java)) {
-            return PhotosOverviewViewModel(application) as T
+            return PhotosOverviewViewModel(application, photo) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
