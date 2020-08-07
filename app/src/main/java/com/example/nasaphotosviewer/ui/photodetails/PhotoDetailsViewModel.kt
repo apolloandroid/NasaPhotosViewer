888303993630a7f8b33fd085ba10f0e21b4783bb
apolloandroid <hmelevskih94@gmail.com>
2020-08-07 12:24:58 +0300

    Created PhotoDetailsViewModel class

---
+++ app/src/main/java/com/example/nasaphotosviewer/ui/photodetails/PhotoDetailsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.nasaphotosviewer.ui.photodetails
+
+import androidx.lifecycle.AndroidViewModel
+import com.example.nasaphotosviewer.App
+
+class PhotoDetailsViewModel(private val application: App) : AndroidViewModel(application) {
+
+}
