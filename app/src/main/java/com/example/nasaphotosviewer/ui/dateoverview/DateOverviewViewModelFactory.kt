bea31bf95ba3960747d480a132770af1ef25d7df
apolloandroid <hmelevskih94@gmail.com>
2020-08-07 12:22:58 +0300

    Deleted unused import classes

--- app/src/main/java/com/example/nasaphotosviewer/ui/dateoverview/DateOverviewViewModelFactory.kt
+++ app/src/main/java/com/example/nasaphotosviewer/ui/dateoverview/DateOverviewViewModelFactory.kt
@@ -1,6 +1,5 @@
 package com.example.nasaphotosviewer.ui.dateoverview
 
-import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.nasaphotosviewer.App
