a2f6a87e5c67c9410cda7e1cb86135889efdfd60
apolloandroid <hmelevskih94@gmail.com>
2020-08-07 12:24:37 +0300

    Created PhotoDetailsFragment

--- app/src/main/java/com/example/nasaphotosviewer/ui/photodetails/PhotoDetailsFragment.kt
+++ app/src/main/java/com/example/nasaphotosviewer/ui/photodetails/PhotoDetailsFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.nasaphotosviewer.App
 import com.example.nasaphotosviewer.R
+import com.example.nasaphotosviewer.databinding.FragmentPhotoDetailsBinding
 
 
 class PhotoDetailsFragment : Fragment() {
+
+    private val viewModel: PhotoDetailsViewModel by lazy { initViewModel() }
+    private lateinit var binding: FragmentPhotoDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_photo_details, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_photo_details, container, false)
+        initViewModel()
+        return binding.root
+    }
+
+    private fun initViewModel(): PhotoDetailsViewModel {
+        val application = App()
+        val photoDetailViewModelFactory = PhotoDetailsViewModelFactory(application)
+        return photoDetailViewModelFactory.create(PhotoDetailsViewModel::class.java)
     }
 }
