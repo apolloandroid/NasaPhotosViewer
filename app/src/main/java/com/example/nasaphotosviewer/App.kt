7cf251f2bb1cbee0f95c2c18df4401adc72c5922
apolloandroid <hmelevskih94@gmail.com>
2020-08-08 23:18:12 +0300

    Deleted initialization of UniversalImageLoader

--- app/src/main/java/com/example/nasaphotosviewer/App.kt
+++ app/src/main/java/com/example/nasaphotosviewer/App.kt
@@ -3,28 +3,7 @@ package com.example.nasaphotosviewer
 import android.app.Application
 import com.example.nasaphotosviewer.data.NasaRepository
 import com.example.nasaphotosviewer.data.Repository
-import com.example.nasaphotosviewer.data.network.NasaService
-import com.example.nasaphotosviewer.data.network.NetworkService
-import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache
-import com.nostra13.universalimageloader.core.DisplayImageOptions
-import com.nostra13.universalimageloader.core.ImageLoader
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
 
 class App : Application() {
     val repository: Repository = NasaRepository()
-
-    override fun onCreate() {
-        super.onCreate()
-
-        val displayImageOptions = DisplayImageOptions.Builder()
-            .cacheInMemory(true)
-            .build()
-        val config = ImageLoaderConfiguration.Builder(this)
-            .defaultDisplayImageOptions(displayImageOptions)
-            .memoryCache(LruMemoryCache(20 * 1024 * 1024))
-            .memoryCacheSize(20 * 1024 * 1024)
-            .build()
-
-        ImageLoader.getInstance().init(config)
-    }
 }
