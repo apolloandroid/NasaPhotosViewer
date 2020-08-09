7dcc8fb8ed9ce7ab20146692d24b02e8a87b19be
apolloandroid <hmelevskih94@gmail.com>
2020-08-09 03:17:26 +0300

    Added onCreateOptionsMenu() method

--- app/src/main/java/com/example/nasaphotosviewer/MainActivity.kt
+++ app/src/main/java/com/example/nasaphotosviewer/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.nasaphotosviewer
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_photo_fragment, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
