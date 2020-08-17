8eb81a0bae29393c4519503f86a36b2d461bd81c
apolloandroid <hmelevskih94@gmail.com>
2020-08-17 23:24:25 +0300

    Added companion object method getInstance()

--- app/src/main/java/com/example/nasaphotosviewer/data/NasaRepository.kt
+++ app/src/main/java/com/example/nasaphotosviewer/data/NasaRepository.kt
@@ -1,11 +1,22 @@
 package com.example.nasaphotosviewer.data
 
+import android.app.Application
 import com.example.nasaphotosviewer.data.model.Date
 import com.example.nasaphotosviewer.data.model.Photo
 import com.example.nasaphotosviewer.data.network.NasaService
 import com.example.nasaphotosviewer.data.network.NetworkService
 
 class NasaRepository : Repository {
+    companion object {
+        fun getInstance(application: Application): Repository {
+            var instance: Repository? = null
+            if (instance == null) {
+                instance = NasaRepository()
+            }
+            return instance
+        }
+    }
+
     private val networkService: NetworkService =
         NasaService()
 
